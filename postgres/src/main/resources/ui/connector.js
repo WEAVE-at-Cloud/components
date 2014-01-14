3f34db49a8c0b664d19a4355282f1123c2088686
tfennelly <tom.fennelly@gmail.com>
2014-01-14 20:38:34 +0000

    Added sslfactory config to postgres

--- postgres/src/main/resources/ui/connector.js
+++ postgres/src/main/resources/ui/connector.js
@@ -1,6 +1,19 @@
 $.foxweave.addComponentView(function() {
     var component = this;
 
+    component.onAccountAdded(function(e) {
+        if (e.accountInfo !== undefined) {
+
+            if (e.accountInfo.ssltoggle && e.accountInfo.ssltoggle === 'true') {
+                e.accountInfo.ssl = true;
+                e.accountInfo.sslfactory = "org.postgresql.ssl.NonValidatingFactory";
+            } else {
+                delete e.accountInfo.ssl;
+                delete e.accountInfo.sslfactory;
+            }
+        }
+    });
+
     component.onAccountChanged(function() {
         setRdbUrl();
     });
